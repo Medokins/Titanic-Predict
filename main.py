6eec9b1e0e8619bf99e5bb53b2af69ba295a85ca
Medokins <nikodemszwast@gmail.com>
2022-01-15 23:20:30 +0100

    fix model testing

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+from cgi import test
 import pickle
 import pandas as pd
+import numpy as np
+from ForTheCurious.featureEngineering import preprocessData
 
 testDf = pd.read_csv("ForTheCurious/datasets/test.csv")
+testDf.drop("Ticket", axis=1, inplace=True)
+testDf.drop("Cabin", axis=1, inplace=True)
+testDf.drop("Embarked", axis=1, inplace=True)
+
+preprocessData(testDf)
+testDf.dropna(inplace=True)
+
+testDf = testDf.astype(float)
+
+print(testDf.dtypes)
 
 model = pickle.load(open('model.pkl', 'rb'))
 predictionDict = {"PassengerId":[], "Survived": []}
 
 for i in range(len(testDf)):
-    predictionDict["PassengerId"].append(testDf["PassengerId"][i])
-    predictionDict["Survived"].append(model.predict([testDf.iloc[i][1:]])[0])
-    # print("Passanger: ", testDf["PassengerId"][i], "Survived: ", clf.predict([testDf.iloc[i][1:]]))
+    predictionDict["PassengerId"].append(np.int32(testDf["PassengerId"][i]))
+    predictionDict["Survived"].append(np.int32(model.predict([testDf.iloc[i][1:]])[0]))
+    print("Passanger: ", testDf["PassengerId"][i], "Survived: ", np.int32(model.predict([testDf.iloc[i][1:]])))
 
 
 predictionDataFrame = pd.DataFrame(predictionDict)
-predictionDataFrame.to_csv("C:/Users/medok/OneDrive/Desktop/Titanic/Prediction.csv", index=False)
+predictionDataFrame.to_csv("Prediction.csv", index=False)
