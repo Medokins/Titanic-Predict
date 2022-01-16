b905002f3f060fd3a81846149d4cc44d37c5b348
Medokins <nikodemszwast@gmail.com>
2022-01-16 22:05:17 +0100

    clean code, format both test and train Df

--- ForTheCurious/featureEngineering.py
+++ ForTheCurious/featureEngineering.py
@@ -6,27 +6,12 @@ def preprocessData(train_df, test_df):
 
     train_df.drop(['PassengerId'], axis=1, inplace=True)
     train_df.drop(["Ticket"], axis=1, inplace=True)
-
     test_df.drop(["Ticket"], axis=1, inplace=True)
 
-    titleList = ['Mrs', 'Mr', 'Master', 'Miss', 'Major', 'Rev', 'Dr', 'Ms', 'Mlle','Col', 'Capt', 'Mme', 'Countess', 'Don', 'Jonkheer']
-    titleDict = {
-    "Mrs": 2,
-    "Mr": 1,
-    "Master": 1,
-    "Miss": 2,
-    "Major": 1,
-    "Rev": 2,
-    "Dr": 1,
-    "Ms": 1,
-    "Mlle": 2,
-    "Col": 1,
-    "Capt": 1,
-    "Mme": 2,
-    "Countess": 3,
-    "Don": 1,
-    "Jonkheer": 1
-    }
+    titles = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+
+    genders = {"male": 0, "female": 1}
+
     deck = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "U": 8}
     data = [train_df, test_df]
 
@@ -43,7 +28,7 @@ def preprocessData(train_df, test_df):
         dataset['Deck'] = dataset['Deck'].fillna(0)
         dataset['Deck'] = dataset['Deck'].astype(np.float)
 
-        #using and fillingNa in age feature
+        #using and filling NaN in age feature
         mean = train_df["Age"].mean()
         std = test_df["Age"].std()
         is_null = dataset["Age"].isnull().sum()
@@ -57,37 +42,45 @@ def preprocessData(train_df, test_df):
         dataset['Embarked'] = dataset['Embarked'].fillna(commonValue)
         dataset['Embarked'] = dataset['Embarked'].map(ports)
 
-    for name in train_df["Name"]:
-        for title in titleList:
-            if title in name:
-                train_df.at[i, "Name"] = titleDict[title]
-                break
-        if train_df["Sex"][i] == "male":
-            train_df.at[i, "Sex"] = 0
-        elif train_df["Sex"][i] == "female":
-            train_df.at[i, "Sex"] = 1
+        #filling Fare
+        dataset['Fare'] = dataset['Fare'].fillna(0)
+        dataset['Fare'] = dataset['Fare'].astype(int)
 
-        if type(train_df["Fare"][i]) != float:
-            train_df.at[i, "Fare"] = 0
-        i += 1
-    
-    i = 0
+        #using Name feature, converting name to Int
+        dataset['Title'] = dataset.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+        dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr',\
+                                                'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+        dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
+        dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+        dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
 
-    for name in test_df["Name"]:
-        for title in titleList:
-            if title in name:
-                test_df.at[i, "Name"] = titleDict[title]
-                break
-        if test_df["Sex"][i] == "male":
-            test_df.at[i, "Sex"] = 0
-        elif test_df["Sex"][i] == "female":
-            test_df.at[i, "Sex"] = 1
+        dataset['Title'] = dataset['Title'].map(titles)
+        dataset['Title'] = dataset['Title'].fillna(0)
 
-        if type(test_df["Fare"][i]) != float:
-            test_df.at[i, "Fare"] = 0
+        #using the Sex feature
+        dataset['Sex'] = dataset['Sex'].map(genders)
 
-            i += 1
+        #creating categories for age feature
+        dataset['Age'] = dataset['Age'].astype(int)
+        dataset.loc[ dataset['Age'] <= 11, 'Age'] = 0
+        dataset.loc[(dataset['Age'] > 11) & (dataset['Age'] <= 18), 'Age'] = 1
+        dataset.loc[(dataset['Age'] > 18) & (dataset['Age'] <= 22), 'Age'] = 2
+        dataset.loc[(dataset['Age'] > 22) & (dataset['Age'] <= 27), 'Age'] = 3
+        dataset.loc[(dataset['Age'] > 27) & (dataset['Age'] <= 33), 'Age'] = 4
+        dataset.loc[(dataset['Age'] > 33) & (dataset['Age'] <= 40), 'Age'] = 5
+        dataset.loc[(dataset['Age'] > 40) & (dataset['Age'] <= 66), 'Age'] = 6
+        dataset.loc[ dataset['Age'] > 66, 'Age'] = 6
 
-    
+        #creating categories for Fare feature
+        dataset.loc[ dataset['Fare'] <= 7.91, 'Fare'] = 0
+        dataset.loc[(dataset['Fare'] > 7.91) & (dataset['Fare'] <= 14.454), 'Fare'] = 1
+        dataset.loc[(dataset['Fare'] > 14.454) & (dataset['Fare'] <= 31), 'Fare']   = 2
+        dataset.loc[(dataset['Fare'] > 31) & (dataset['Fare'] <= 99), 'Fare']   = 3
+        dataset.loc[(dataset['Fare'] > 99) & (dataset['Fare'] <= 250), 'Fare']   = 4
+        dataset.loc[ dataset['Fare'] > 250, 'Fare'] = 5
+        dataset['Fare'] = dataset['Fare'].astype(int)
+
     train_df.drop(['Cabin'], axis=1, inplace=True)
     test_df.drop(['Cabin'], axis=1, inplace=True)
+    train_df.drop(['Name'], axis=1, inplace=True)
+    test_df.drop(['Name'], axis=1, inplace=True)
